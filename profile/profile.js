882806462ca28868e0e646e4d6e708a309aafccc
edrivvoll <espendr@outlook.com>
2024-12-15 22:30:26 +0100

    search clean up

--- profile/profile.js
+++ profile/profile.js
@@ -1,29 +1,9 @@
 import { logOut } from "../js/logOut.js";
 import { goToCreatePost } from "../js/goToCreatePost.js";
 import { renderPostsWithFilterMyPosts } from "../js/renderPostsWithFilterMyPosts.js"
-import { renderSearchProfile } from "../js/searchPostsProfile.js";
 
 renderPostsWithFilterMyPosts();
 
 logOut();
 
 goToCreatePost();
-
-
-const searchBtn = document.getElementById('search-btn');
-const searchInput = document.querySelector('#search-input');
-
-const url = '';
-
-searchBtn.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    if (searchInput.value === '') {
-        renderPostsWithFilterMyPosts();
-    }   else {
-        renderSearchProfile(searchInput.value);
-    }
-    
-});
-
-
-
